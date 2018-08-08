bc1b2cd43181be55a8d7808cb1e3b86a37c877cd
triador <triador.sub@yandex.ru>
2018-08-08 15:51:30 +0300

    add Observer pattern

---
+++ patterns/src/main/java/com/triador/strategy/FlyBehavior.java
@@ -0,0 +1,5 @@
+package com.triador.strategy;
+
+public interface FlyBehavior {
+    public void fly();
+}
---
+++ patterns/src/main/java/com/triador/strategy/QuackBehavior.java
@@ -0,0 +1,5 @@
+package com.triador.strategy;
+
+public interface QuackBehavior {
+    public void quack();
+}
