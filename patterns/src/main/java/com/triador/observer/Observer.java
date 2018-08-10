e1e3d29d5c7876bfddb69aaf3907873b309a4b74
triador <triador.sub@yandex.ru>
2018-08-10 14:49:51 +0300

    add Observer pattern

--- patterns/src/main/java/com/triador/observer/Observer.java
+++
@@ -1,6 +0,0 @@
-package com.triador.observer;
-
-public abstract class Observer {
-    protected Subject subject;
-    public abstract void update();
-}
---
+++ patterns/src/main/java/com/triador/observer/weather/DisplayElement.java
@@ -0,0 +1,5 @@
+package com.triador.observer.weather;
+
+public interface DisplayElement {
+    public void display();
+}
